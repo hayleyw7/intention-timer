1f3f10299077e5f077d3addcfa78dfb61e8562f9
Alex Kio <alexandermartelkio@gmail.com>
2021-04-13 16:47:47 -0400

    Update Activity class skeleton
    needs startTimer to be real code, not pseudo

--- Activity.js
+++ Activity.js
@@ -8,12 +8,25 @@ class Activity {
     this.id = id;
   }
   startTimer() {
-
+    // cause time to countdown
+    // remove START text within circle
+    // markComplete() (updates data model)
+    // alert when done
+    // congratulations screen with log activity button appears after alert goes away
+    //^I'm guessing call a function AFTER a certain amount of time (updates DOM)
+    //^^I have a showCongrats() in the js to inject the html for this
   }
+  // updates data model
   markComplete() {
     this.completed = true;
   }
+  // updates data model
   saveToStorage() {
     activities.push(currentActivity)
+    // updates DOM
+    // updates right side with our newly saved activity (for loop)
+    updatePastActivitiesSection()
+    // adds and removes a hidden class in the HTML
+    showCompletedActivitiesSection()
   }
 }
