a9a8bf9d53e85b5ad82661ac00ec023aba149bb2
Alex Kio <alexandermartelkio@gmail.com>
2021-04-13 16:46:01 -0400

    Update js skeleton
    Added event bubbling, and an event bubbling if/else filter

--- main.js
+++ main.js
@@ -1,58 +1,76 @@
 // all the query selectors and eventListeners
 
+// for showing the errors if no value is entered - are you all going to need these to innerHTML?
 var descriptionErrorSection = document.querySelector('');
-
+descriptionErrorSection.addEventListener('click', functionName);
 var timeErrorSection = document.querySelector('');
+timeErrorSection.addEventListener('click', functionName);
 
 var startActivityButton = document.querySelector('.start-btn');
 startActivityButton.addEventListener('click', createActivity);
 
 var activitiesSection = document.querySelector('#new-container');
-var pastActivitiesSection = document.querySelector('#');
+
+activitiesSection.addEventListener('click', filterEventBubbles);
+
 // I need an element below the h2... that encapsulates the <p>
-
-var startCircle = document.querySelector('');
-startCircle.addEventListener('click', startCountdown);
-
-var logActivityButton = document.querySelector('');
-logActivityButton.addEventListener('click', logActivity);
+var pastActivitiesSection = document.querySelector('#');
 
 var startNewActivityButton = document.querySelector('');
 startNewActivityButton.addEventListener('click', startNewActivity);
 
 // global variables
-
 var currentActivity;
 var activities = [];
 
 // PART 1 Create Activity --> Show Start
 
+// create a random id number for the instance of Activity class below it
 function generateRandomID() {
   var id = Math.floor((1 + Math.random()) * 0x100000)
   return id
 }
 
 // update Data Model
+// when start activity button is clicked currentactivity variable is assigned a new instance of the Activity class
+// and displayCountdown() is called
 function createActivity() {
   // var id = generateRandomID()
-  currentActivity = new Cover(category.value, description.value, minutes.value, seconds.value, generateRandomID());
+  currentActivity = new Activity(category.value, description.value, minutes.value, seconds.value, generateRandomID());
   displayCountdown();
 }
 
 // Update DOM
+// injects dynamic html à la https://frontend.turing.io/projects/module-1/assets/intention-timer/timer-start-desktop.png
 function displayCountdown() {
-  activitiesSection.innerHTML = ``
+  activitiesSection.innerHTML = `
+  ${currentActivity.description}
+  ${currentActivity.minutes}:${currentActivity.seconds}
+  // start button with id="startBTN"
+  `
 }
 
-// startTimer method in class
-
-//saveToStorage method in class
-
-function startNewActivity() {
-  activitiesSection.innerHTML = ``
-  updatePastActivitiesSection()
+// assigns specific events on the same parent to specific functions
+function filterEventBubbles() {
+  if (e.target.id === "startBTN") {
+    currentActivity.startTimer()
+  } else if (e.target.id === "logBTN") {
+    currentActivity.saveToStorage()
+  }
 }
 
+// Update DOM
+// injects dynamic html à la https://frontend.turing.io/projects/module-1/assets/intention-timer/complete-timer-desktop.png
+function showCongrats() {
+  activitiesSection.innerHTML = `
+  ${currentActivity.description}
+  00:00
+  COMPLETE!
+  // log activity button with id="logBTN"
+  `
+}
+
+// local storage!!
 function updatePastActivitiesSection() {
   for (var i = 0; i < activities.length; i++) {
     pastActivitiesSection.innerHTML += `
@@ -60,4 +78,14 @@ function updatePastActivitiesSection() {
     ${activities[i].minutes} MIN ${activities[i].seconds} SECONDS
     ${activities[i].description}
     `
+}
+
+function showCompletedActivitiesSection() {
+  activitiesSection.classList.add("hidden");
+  completedActivitiesSection.classList.remove("hidden");
+}
+
+function showActivitiesSection() {
+  activitiesSection.classList.remove("hidden");
+  completedActivitiesSection.classList.add("hidden");
 }
