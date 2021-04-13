dbb95c75593834b1ea8eff740f8524e7753dbc5e
Alex Kio <alexandermartelkio@gmail.com>
2021-04-13 10:03:30 -0400

    Create initial Activity class

--- Activity.js
+++ Activity.js
@@ -1,1 +1,19 @@
+class Activity {
+  constructor(category, description, minutes, seconds, id) {
+    this.category = category;
+    this.description = description;
+    this.minutes = minutes;
+    this.seconds = seconds;
+    this.completed = false;
+    this.id = id;
+  }
+  startTimer() {
 
+  }
+  markComplete() {
+    this.completed = true;
+  }
+  saveToStorage() {
+    activities.push(currentActivity)
+  }
+}
