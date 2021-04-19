6effd36348a684b8d6be0df2ddbd11964d161bf8
Alex Kio <alexandermartelkio@gmail.com>
2021-04-19 16:36:08 -0400

    Remove onClicks, Add eventListeners
    bug= card displays again after ring (start timer) click

--- main.js
+++ main.js
@@ -12,13 +12,16 @@ var eOn = document.querySelector('#e-on')
 var eOff = document.querySelector('#e-off')
 
 // Forms
-var hiddenInput = document.querySelector('#hidden-input')
 var studyLabel = document.querySelector('#study-label')
 var meditateLabel = document.querySelector('#meditate-label')
 var exerciseLabel = document.querySelector('#exercise-label')
 var startActivityBtn = document.querySelector('#start-btn')
 var logActivityBtn = document.querySelector('#log-btn')
 var createNewActivityBtn = document.querySelector('#create-new-activity-btn')
+var categoryValue = document.querySelector('#category')
+var goalValue = document.querySelector('#goal')
+var minutesValue = document.querySelector('#minutes')
+var secondsValue = document.querySelector('#seconds')
 
 // Outer Card
 var card = document.querySelector("#card-content")
@@ -41,19 +44,20 @@ var goalWarning = document.querySelector('#warning-1')
 var minutesWarning = document.querySelector('#warning-2')
 var secondsWarning = document.querySelector('#warning-3')
 
-
-
 // Event Listeners 
 window.addEventListener('load', createActivityCard)
 studyBtn.addEventListener('click', activateStudy);
 meditateBtn.addEventListener('click', activateMeditate);
 exerciseBtn.addEventListener('click', activateExcercise);
-ring.addEventListener('click', triggerTimer)
+ring.addEventListener('click', callTimerMethod);
+startActivityBtn.addEventListener('click', createActivity);
+logActivityBtn.addEventListener('click', logActivity);
+ring.addEventListener('click', makeNewActivity)
 
 
 // Event Handlers
 function activateStudy() {
-  hiddenInput.innerHTML = `<input type="hidden" name="category" value="Study">`
+  categoryValue.id = "category study"
   studyBtn.style.borderColor = ('var(--study)')
   ring.style.borderColor = ('var(--study)')
   studyLabel.style.color = ('var(--study)')
@@ -70,7 +74,7 @@ function activateStudy() {
 }
 
 function activateMeditate() {
-  hiddenInput.innerHTML = `<input type="hidden" name="category" value="Meditate">`
+  categoryValue.id = "category meditate"
   meditateBtn.style.borderColor = ('var(--meditate)')
   ring.style.borderColor = ('var(--meditate)')
   meditateLabel.style.color= ('var(--meditate')
@@ -87,7 +91,7 @@ function activateMeditate() {
 }
 
 function activateExcercise() {
-  hiddenInput.innerHTML = `<input type="hidden" name="category" value="Exercise">`
+  categoryValue.id = "category exercise"
   exerciseBtn.style.borderColor = ('var(--exercise)')
   ring.style.borderColor = ('var(--exercise)')
   exerciseLabel.style.color= ('var(--exercise')
@@ -104,13 +108,21 @@ function activateExcercise() {
   hide(sOn)
 }
 
-function createActivity(form) {
-  !hiddenInput.innerHTML ? displayWarning(catagoryWarning) : hide(catagoryWarning)
-  form.goal.value === "" ? displayWarning(goalWarning) : hide(goalWarning)
-  !form.minutes.value ? displayWarning(minutesWarning) : hide(minutesWarning)
-  !form.seconds.value ? displayWarning(secondsWarning) : hide(secondsWarning)
-  if(form.goal.value && form.minutes.value && form.seconds.value) {
-    currentActivity = new Activity(form.category.value, form.goal.value, parseInt(form.minutes.value), parseInt(form.seconds.value), generateRandomID());
+function createActivity() {
+  var selectedCategory;
+  if (categoryValue.id === "category study") {
+    selectedCategory = "Study"
+  } else if (categoryValue.id === "category meditate") {
+    selectedCategory = "Meditate"
+  } else if (categoryValue.id === "category exercise") {
+    selectedCategory = "Exercise"
+  }
+  categoryValue.id === "category" ? displayWarning(catagoryWarning) : hide(catagoryWarning)
+  goalValue.value === "" ? displayWarning(goalWarning) : hide(goalWarning)
+  !minutesValue.value ? displayWarning(minutesWarning) : hide(minutesWarning)
+  !secondsValue.value ? displayWarning(secondsWarning) : hide(secondsWarning)
+  if(selectedCategory && goalValue.value && minutesValue.value && secondsValue.value) {
+    currentActivity = new Activity(selectedCategory, goalValue.value, parseInt(minutesValue.value), parseInt(secondsValue.value), generateRandomID());
     showTimer()
     show(timerCard)
     hide(card)
@@ -129,6 +141,10 @@ function showTimer() {
   var seconds = currentActivity.seconds < 10 ? "0" + currentActivity.seconds : currentActivity.seconds;
   activityHeader.textContent = `${currentActivity.description}`
   timeLeft.textContent = minutes + ":" + seconds;
+}
+
+function callTimerMethod() {
+  currentActivity.startTimer();
 }
 
 function beginTimer(minutes, seconds) {
@@ -182,7 +198,6 @@ function createActivityCard() {
       </div>
       <h5 id="past-card-goal">${activities[i].description}</h5>
       </div>`
-      console.log(pastActivitiesCard)
     }
   }
   return activities
@@ -220,17 +235,12 @@ function updateHeader() {
   activityHeader.innerText = "Current Activity"
 }
 
-function triggerTimer() {
-  beginTimer(currentActivity.minutes, currentActivity.seconds)
-}
-
 function changeCategoryColor(category) {
 var color = ""
 category === 'Study' ? color = "green" 
 : category === 'Meditate' ? color = "purple" 
 : category === 'Exercise' ? color = "red" 
 : color="var(--white)"
-console.log(color)
 return color
 }
 
