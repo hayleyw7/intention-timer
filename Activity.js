8a31a3a56e1dd6c52b10af222dfea72e781c4d3f
Alex Kio <alexandermartelkio@gmail.com>
2021-04-23 11:19:32 -0400

    Pseudocode

--- Activity.js
+++ Activity.js
@@ -8,9 +8,15 @@ class Activity {
     this.id = id;
   }
   startTimer() {
+    // assigns timer an instance of CountDownTimer passing in minutes and seconds
     timer = new CountDownTimer(this.minutes, this.seconds);
+    // calls onTick function of timer passing in format fn, onTick restart(), and start()
     timer.onTick(format(timeLeft)).onTick(restart).start();
     function restart() {
+      // if the timer cas expired (if this.running = false [line 58 of timer.js]) call markComplete method
+      // update DOM with log activity button
+      // change timer innerText to COMPLETE
+      // return alert
       if (this.expired()) {
         setTimeout(function () {
           currentActivity.markComplete();
@@ -22,8 +28,10 @@ class Activity {
     }
     function format(timeLeft) {
       return function (minutes, seconds) {
+        // assign minutes to minutes if it's double digits, or add leading 0 if it's single digit
         minutes = minutes < 10 ? '0' + minutes : minutes;
         seconds = seconds < 10 ? '0' + seconds : seconds;
+        //update DOM with countdown of minutes & seconds
         timeLeft.textContent = minutes + ':' + seconds;
       };
     }
--- timer.js
+++ timer.js
@@ -7,17 +7,28 @@ function CountDownTimer(minutes, seconds) {
 }
   
 CountDownTimer.prototype.start = function() {
+  // if running = true break out of fn
   if (this.running) {
     return;
   }
+  // otherwise change running to true
   this.running = true;
+  // assign start variable to Date/Time now
   var start = Date.now(),
       that = this,
       diff, obj;
 
   (function timer() {
+    // assign diff to durration of timer (user imputed, converted to total number of seconds)
+    // minus the difference between new Datenow and start Datenow
+    //divided by 1000ms
+    //if there is no difference make it 0
+    //this is the meat and potatoes of the timer
     diff = that.duration - (((Date.now() - start) / 1000) | 0);
     
+    // if difference is greater than 0 seconds call setTimeout function
+    // passing timer (instantiation of CountDownTimer) and granularity (1000ms refresh)
+    //otherwise assign diff 0 and make running = false
     if (diff > 0) {
       setTimeout(timer, that.granularity);
     } else {
@@ -33,9 +44,13 @@ CountDownTimer.prototype.start = function() {
 };
   
 CountDownTimer.prototype.onTick = function(ftn) {
+// ftn = format(timeLeft) (line25 of Activity.js)
   if (typeof ftn === 'function') {
+    // tickFtns = restart() (line 15 of Activity.js)
+    // push makes tickFtns an array of the two functions above
     this.tickFtns.push(ftn);
   }
+  // this = CountDownTimer class
   return this;
 };
 
